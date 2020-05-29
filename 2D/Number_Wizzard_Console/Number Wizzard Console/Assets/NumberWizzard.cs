e2da50bc053ed5a0780451df66d03b36cd830ec0
dbartalos <6267612+dbartalos@users.noreply.github.com>
2020-05-29 21:14:03 +0100

    adds number wizzard console

---
+++ 2D/Number_Wizzard_Console/Number Wizzard Console/Assets/NumberWizzard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizzard : MonoBehaviour
+{
+
+    int max;
+    int min;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        Debug.Log("Welcome to number wizzard, yo");
+        Debug.Log("Pick a number");
+        Debug.Log("Highest number is: " + max);
+        Debug.Log("Lowest number is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Press Up = higher, Press Down = lower, Enter = Correct");
+        max++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        //Detect when the down arrow key is pressed down
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I am a genius!");
+            StartGame();
+        }
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is your number is higher or lower than " + guess);
+    }
+}
