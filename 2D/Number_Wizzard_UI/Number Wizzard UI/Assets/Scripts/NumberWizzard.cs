75c62d692ed8b04f1d3076ee6283250f66091c7d
dbartalos <6267612+dbartalos@users.noreply.github.com>
2020-06-06 21:47:09 +0100

    adds NumberWizzardUI

---
+++ 2D/Number_Wizzard_UI/Number Wizzard UI/Assets/Scripts/NumberWizzard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class NumberWizzard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1024, 768, false);
+        StartGame();        
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        max++;
+    }
+
+    public void OnPressHigher() {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower() {
+        max = guess;
+        NextGuess();
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }    
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
